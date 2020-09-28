ebe5236c8e3bffca337409cbf575d54a79fa0d78
SC_Fighting <hyswcvip@163.com>
2020-09-28 15:04:32 +0800

    gpu

--- Class/Home/Controller/HomeViewController.m
+++ Class/Home/Controller/HomeViewController.m
@@ -7,11 +7,45 @@
 //
 
 #import "HomeViewController.h"
-
+#import <GPUImage/GPUImage.h>
 @interface HomeViewController ()
+@property(nonatomic,strong)GPUImageVideoCamera *videoCamera;
+@property(nonatomic,strong)GPUImageView *displayView;
 @end
 
 @implementation HomeViewController
 
+-(void)loadView
+{
+    [super loadView];
+    [self.navigationController setNavigationBarHidden:YES];
+    [self.view addSubview:self.displayView];
+    [self.displayView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(self.view);
+    }];
+    [self.videoCamera addTarget:self.displayView];
+    [self.videoCamera startCameraCapture];
+}
+
+#pragma mark -- getter
+
+-(GPUImageVideoCamera *)videoCamera
+{
+    if (_videoCamera == nil) {
+        _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1280x720 cameraPosition:AVCaptureDevicePositionFront];
+        [_videoCamera setOutputImageOrientation:UIInterfaceOrientationPortrait];
+        _videoCamera.horizontallyMirrorFrontFacingCamera = YES;
+    }
+    return _videoCamera;
+}
+
+-(GPUImageView *)displayView
+{
+    if (_displayView == nil) {
+        _displayView = [[GPUImageView alloc] init];
+        _displayView.fillMode = kGPUImageFillModePreserveAspectRatioAndFill;
+    }
+    return _displayView;
+}
 
 @end
